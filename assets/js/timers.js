e6fa1626cf6faf79de3806ec3a51cd20f27beaee
Dameon Brown <axel12gt@live.com>
2019-06-07 07:39:25 -0400

    added timer functions

--- assets/js/timers.js
+++ assets/js/timers.js
@@ -1,36 +1,47 @@
 $(document).ready()
-var q = [], a1 = [], a2= [], a3 = [], a4 = [], correctAnswers = []
+//creates empty arrays for questions and answers to push to
+var q = [], a1 = [], a2= [], a3 = [], a4 = []
+// for question and answer array traversal
 var iterator = 0
+// interval variable for timer function
+var intervalID
+//sets timer to 30 secons
+var number =30
+//boolean for timer control initialized to false
+var running =  false
+//correct and incorrect answer count
+var correct = 0, incorrect = 0, noAnswer = 0
+
 
 var gameObject = {
 
     questionsIterator:function(){
         // checks to see if we reached the end of the array
         //calls the object questions and iterates through trivia questions
-        $(".questions").html("<b>"+ q[0] + "</b><br>")
+        $(".questions").html("<b>"+ q[0] + "</b>")
         //calls the answers
         //TODO: randomize answer choices
-        $(".answers1").html("<b>"+ a1[iterator] +"</b><br>")
-        $(".answers2").html("<b>"+ a2[iterator] +"</b><br>")
-        $(".answers3").html("<b>"+ a3[iterator] +"</b><br>")
-        $(".answers4").html("<b>"+ a4[iterator] +"</b><br>")
+        $(".answers1").html("<b>"+ a1[iterator] +"</b>")
+        $(".answers2").html("<b>"+ a2[iterator] +"</b>")
+        $(".answers3").html("<b>"+ a3[iterator] +"</b>")
+        $(".answers4").html("<b>"+ a4[iterator] +"</b>")
         
         
     },
-
+//calls the apis function
     jax: function(questions){
         // calls api
         $.ajax({
+            //url for the api
             url: "https://opentdb.com/api.php?amount=10",
+            //ajax method for recieving information for the api
             method: "GET"
         }).then(function(response){
-            console.log(response)
             // creates a for loop for the 10 things I want to pull from the api
             for(var i =0; i<10;i++){
             // creates variables for the questions and all the answers and calls them from the api
              q.push(response.results[i].question)
              a1.push(response.results[i].correct_answer)
-             correctAnswers.push(response.results[i].correct_answer)
              a2.push(response.results[i].incorrect_answers[0])
              a3.push(response.results[i].incorrect_answers[1])
              a4.push(response.results[i].incorrect_answers[2])
@@ -40,10 +51,58 @@ var gameObject = {
              gameObject.questionsIterator()
         })
     },
-    
+    //sets up timer function
+    /* 
+    =================================
+    NEED THIS TO START THE GAME!!!!!!
+    MAIN METHODS CALLER!!!!!!!!!!!!!!
+    =================================
+    */
+    timer:function(){
+        //checks if timeris running if not  sets interval for timer and sets running to true
+        if(!running){
+            // sets timer for 1 second intervals activating function decremeant each time
+            intervalID = setInterval(this.decrement,1000)
+            //sets running to true so we don't accidently mess with intervals
+            running = true
+        }
+    },
+    //Timer functions partner for decreasing the timer
+    decrement:function(){
+        // decreases variable by 1
+        number--
+        // displays the seconds to the screen 
+        $(".seconds").html("<h2>"+ number + "<h2>")
+        //checks if number = 0 and gives commands after that
+        if(number == 0){
+            // calls stop function to stop the tiemr
+            this.stop()
+            //iterator for questions and answers increases by 1
+            //chooses next question
+            iterator++
+            //increases noAnswer by 1
+            noAnswer++
+            //calls question iterator to display next question
+            this.questionsIterator()
+        }
+    },
+    //Timer reset
+    timerReset:function(){
+        //resets timer to 30 seconds
+        number = 30
+    },
+    //Timer stop
+    stop:function(){
+        // stops timer
+        clearInterval(intervalID)
+        //sets running to false
+        running = false
+    }
 
 }
 gameObject.jax()
+
+
 
 
 
